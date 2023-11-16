fb3b3b71d41cd7d0857b55c42ab3aa72d12b5830
Technominds <harpreetsinghm786@gmail.com>
2023-11-17 03:34:03 +0530

    report

--- app/Http/Livewire/Menulink.php
+++ app/Http/Livewire/Menulink.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire;
 use App\Models\AllUserMapping;
 use App\Models\DBVMappings;
 use App\Models\Patient;
+use App\Models\User;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
@@ -20,10 +21,39 @@ class Menulink extends Component
         "delivered"=>0,
     ];
     public $block="";
-    public function mount(){
-        $v=AllUserMapping::where('smo_id',Auth::user()->id)->first();
-        $this->block=DBVMappings::where('village_id',$v->village_id)->first();
+    public $repo=[];
+    public $reportmodal=false;
+    public function toggle(){
+        $this->reportmodal=!$this->reportmodal;
+    }
+    public function report()
+    {
+        if(Auth::user()->role->id==5){
+        $vill=AllUserMapping::where('smo_id',Auth::user()->id)->pluck('village_id')->toArray();
+        $anms=AllUserMapping::where('smo_id',Auth::user()->id)->distinct()->pluck('anm_id');
+        foreach($anms as $a){
+            $asha=AllUserMapping::where('anm_id',$a)->pluck('asha_id');
+            $u=User::where('id',$a)->get('name')->first();
+            $this->repo[$u->name]=Patient::whereIn('village_id', $vill)->whereIn('asha_id', $asha)->count();
+        }
+    }
+    if(Auth::user()->role->id==6){
+        $vill=AllUserMapping::where('cmo_id',Auth::user()->id)->pluck('village_id')->toArray();
+        $smos=AllUserMapping::where('cmo_id',Auth::user()->id)->distinct()->pluck('smo_id');
+        foreach($smos as $a){
+            $asha=AllUserMapping::where('smo_id',$a)->pluck('asha_id');
+            $u=User::where('id',$a)->get('name')->first();
+            $this->repo[$u->name]=Patient::whereIn('village_id', $vill)->whereIn('asha_id', $asha)->count();
+        }
+    }
 
+        $this->toggle();
+    }
+    public function mount(){
+        //smo
+        if(Auth::user()->role->id==5)
+        {$v=AllUserMapping::where('smo_id',Auth::user()->id)->first();
+        $this->block=DBVMappings::where('village_id',$v->village_id)->first();
         $vill=AllUserMapping::where('smo_id',Auth::user()->id)->pluck('village_id')->toArray();
         $this->stat["total"]=Patient::whereIn('village_id', $vill)->count();
         $anc1days = Carbon::now()->subWeek(12);
@@ -35,6 +65,26 @@ class Menulink extends Component
         $anc4days = Carbon::now()->subMonth(8);
         $this->stat["anc4"]=Patient::whereIn('village_id', $vill)->where('Lmp','<', $anc4days)->whereNull('anc4_date')->count();
         $this->stat["delivered"]=Patient::whereIn('village_id', $vill)->whereNotNull('dateofdelivery')->count();
+        }
+
+         //cmo
+         if(Auth::user()->role->id==6)
+         {
+         $v=AllUserMapping::where('cmo_id',Auth::user()->id)->first();
+         $this->block=DBVMappings::where('village_id',$v->village_id)->first();
+
+         $vill=AllUserMapping::where('cmo_id',Auth::user()->id)->pluck('village_id')->toArray();
+         $this->stat["total"]=Patient::whereIn('village_id', $vill)->count();
+         $anc1days = Carbon::now()->subWeek(12);
+         $this->stat["anc1"]=Patient::whereIn('village_id', $vill)->where('Lmp','<', $anc1days)->whereNull('anc1_date')->count();
+         $anc2days = Carbon::now()->subWeek(28);
+         $this->stat["anc2"]=Patient::whereIn('village_id', $vill)->where('Lmp','<', $anc2days)->whereNull('anc2_date')->count();
+         $anc3days = Carbon::now()->subMonth(7);
+         $this->stat["anc3"]=Patient::whereIn('village_id', $vill)->where('Lmp','<', $anc3days)->whereNull('anc3_date')->count();
+         $anc4days = Carbon::now()->subMonth(8);
+         $this->stat["anc4"]=Patient::whereIn('village_id', $vill)->where('Lmp','<', $anc4days)->whereNull('anc4_date')->count();
+         $this->stat["delivered"]=Patient::whereIn('village_id', $vill)->whereNotNull('dateofdelivery')->count();
+         }
     }
     public function render()
     {
--- resources/views/livewire/menulink.blade.php
+++ resources/views/livewire/menulink.blade.php
@@ -1,9 +1,29 @@
 <div>
         @if(Auth::user()->role->id>4)
-         <div >
-                <span>Reporting Stats - {{$block->block->name}}</span>
+        <div>
+                @if(Auth::user()->role->id==5)
+                <span>Reporting Stats - Block :{{$block->block->name}}</span>
+                <div class="grid grid-cols-2 gap-3 text-white mb-2">
+                        <div class="bg-blue-400 p-3 rounded-lg h-16 flex justify-between">
+                                <span>Total Cases</span><span wire:click="report()" class="font-bold text-white cursor-pointer text-xl">{{$stat["total"]}}</span>
+                        </div>
+                        <div class="bg-green-400 p-3 rounded-lg h-16 flex justify-between">
+                                <span>Delivered Cases</span><span class="font-bold text-white text-xl">{{$stat["delivered"]}}</span>
+                        </div>
+                </div>
+                @elseif(Auth::user()->role->id==6)
+                <span>Reporting Stats - District : {{$block->district->name}}</span>
+                <div class="grid grid-cols-2 gap-3 text-white mb-2">
+                        <div class="bg-blue-400 p-3 rounded-lg h-16 flex justify-between">
+                                <span>Total Cases</span><span wire:click="report()" class="font-bold text-white cursor-pointer text-xl">{{$stat["total"]}}</span>
+                        </div>
+                        <div class="bg-green-400 p-3 rounded-lg h-16 flex justify-between">
+                                <span>Delivered Cases</span><span class="font-bold text-white text-xl">{{$stat["delivered"]}}</span>
+                        </div>
+                </div>
+                @endif
                 <div class="grid mb-2 grid-cols-4 gap-3 text-white">
-                        
+
                         <div class="bg-rose-400 p-3 rounded-lg h-16 flex justify-between">
                                 <span>Pending ANC1</span><span class="font-bold text-white text-xl">{{$stat["anc1"]}}</span>
                         </div>
@@ -16,22 +36,56 @@
                         <div class="bg-rose-400 p-3 rounded-lg h-16 flex justify-between">
                                 <span>Pending ANC4</span><span class="font-bold text-white text-xl">{{$stat["anc4"]}}</span>
                         </div>
-                       
+
 
                 </div>
-                <div class="grid grid-cols-2 gap-3 text-white mb-2">
-                <div class="bg-blue-400 p-3 rounded-lg h-16 flex justify-between">
-                                <span>Total Cases</span><span class="font-bold text-white text-xl">{{$stat["total"]}}</span>
-                        </div>
-                        <div class="bg-green-400 p-3 rounded-lg h-16 flex justify-between">
-                                <span>Delivered Cases</span><span class="font-bold text-white text-xl">{{$stat["delivered"]}}</span>
-                        </div>
-                </div>
-         </div>        
+             
+        </div>
         @endif
 
-                        <div class="mb-2 p-3 bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                                <a href="/mapping" class="bg-pink-800 p-2 text-white rounded-md">Mapping</a>
-                                <a href="/changepwd" class="bg-pink-800 p-2  text-white rounded-md">Change Password</a>
-                        </div>
-                </div>
+        <div class="mb-2 p-3 bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <a href="/mapping" class="bg-pink-800 p-2 text-white rounded-md">Mapping</a>
+                <a href="/changepwd" class="bg-pink-800 p-2  text-white rounded-md">Change Password</a>
+        </div>
+        <x-confirmation-modal wire:model="reportmodal">
+        <x-slot name="icon">
+            
+        </x-slot>
+        <x-slot name="subtitle">
+
+        </x-slot>
+
+        <x-slot name="title">
+            Stats of Team Cases
+        </x-slot>
+
+
+        <x-slot name="content">
+                <table class="min-w-full bg-white border border-gray-300">
+                <thead>
+                <tr>
+                    <th class="py-2 px-4 border-b">Name</th>
+                    <th class="py-2 px-4 border-b">Cases</th>
+                    
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach($repo as $n=>$c)
+                 <tr class="hover:bg-slate-100">
+                        <td class="py-2 px-4 border-b">{{$n}}</td>
+                        <td class="py-2 px-4 border-b">{{$c}}</td>
+                 </tr>
+                @endforeach
+            </tbody>
+                </table>
+        </x-slot>
+        <x-slot name="footer">
+            <x-secondary-button wire:click="toggle()" wire:loading.attr="disabled">
+                Cancel
+            </x-secondary-button>
+
+            
+        </x-slot>
+        </x-confirmation-modal>
+</div>
