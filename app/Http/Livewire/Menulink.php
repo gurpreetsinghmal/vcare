a2334a4cd01f0b372e18fead98e95a1f4da88682
Technominds <harpreetsinghm786@gmail.com>
2023-11-17 02:13:48 +0530

    reports

--- app/Http/Livewire/Menulink.php
+++ app/Http/Livewire/Menulink.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\AllUserMapping;
+use App\Models\DBVMappings;
+use App\Models\Patient;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Menulink extends Component
 {
+    public $stat=[
+        "total"=>0,
+        "anc1"=>0,
+        "anc2"=>0,
+        "anc3"=>0,
+        "anc4"=>0,
+        "delivered"=>0,
+    ];
+    public $block="";
+    public function mount(){
+        $v=AllUserMapping::where('smo_id',Auth::user()->id)->first();
+        $this->block=DBVMappings::where('village_id',$v->village_id)->first();
+
+        $vill=AllUserMapping::where('smo_id',Auth::user()->id)->pluck('village_id')->toArray();
+        $this->stat["total"]=Patient::whereIn('village_id', $vill)->count();
+        $anc1days = Carbon::now()->subWeek(12);
+        $this->stat["anc1"]=Patient::whereIn('village_id', $vill)->where('Lmp','<', $anc1days)->whereNull('anc1_date')->count();
+        $anc2days = Carbon::now()->subWeek(28);
+        $this->stat["anc2"]=Patient::whereIn('village_id', $vill)->where('Lmp','<', $anc2days)->whereNull('anc2_date')->count();
+        $anc3days = Carbon::now()->subMonth(7);
+        $this->stat["anc3"]=Patient::whereIn('village_id', $vill)->where('Lmp','<', $anc3days)->whereNull('anc3_date')->count();
+        $anc4days = Carbon::now()->subMonth(8);
+        $this->stat["anc4"]=Patient::whereIn('village_id', $vill)->where('Lmp','<', $anc4days)->whereNull('anc4_date')->count();
+        $this->stat["delivered"]=Patient::whereIn('village_id', $vill)->whereNotNull('dateofdelivery')->count();
+    }
     public function render()
     {
+
         return view('livewire.menulink');
     }
 }
--- resources/views/livewire/menulink.blade.php
+++ resources/views/livewire/menulink.blade.php
@@ -1,6 +1,37 @@
 <div>
-        <div class="mb-2 p-3 bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <a href="/mapping" class="bg-blue-200 p-2">Mapping</a>
-                <a href="/changepwd" class="bg-blue-200 p-2">Change Password</a>
-            </div>
-</div>
+        @if(Auth::user()->role->id>4)
+         <div >
+                <span>Reporting Stats - {{$block->block->name}}</span>
+                <div class="grid mb-2 grid-cols-4 gap-3 text-white">
+                        
+                        <div class="bg-rose-400 p-3 rounded-lg h-16 flex justify-between">
+                                <span>Pending ANC1</span><span class="font-bold text-white text-xl">{{$stat["anc1"]}}</span>
+                        </div>
+                        <div class="bg-rose-400 p-3 rounded-lg h-16 flex justify-between">
+                                <span>Pending ANC2</span><span class="font-bold text-white text-xl">{{$stat["anc2"]}}</span>
+                        </div>
+                        <div class="bg-rose-400 p-3 rounded-lg h-16 flex justify-between">
+                                <span>Pending ANC3</span><span class="font-bold text-white text-xl">{{$stat["anc3"]}}</span>
+                        </div>
+                        <div class="bg-rose-400 p-3 rounded-lg h-16 flex justify-between">
+                                <span>Pending ANC4</span><span class="font-bold text-white text-xl">{{$stat["anc4"]}}</span>
+                        </div>
+                       
+
+                </div>
+                <div class="grid grid-cols-2 gap-3 text-white mb-2">
+                <div class="bg-blue-400 p-3 rounded-lg h-16 flex justify-between">
+                                <span>Total Cases</span><span class="font-bold text-white text-xl">{{$stat["total"]}}</span>
+                        </div>
+                        <div class="bg-green-400 p-3 rounded-lg h-16 flex justify-between">
+                                <span>Delivered Cases</span><span class="font-bold text-white text-xl">{{$stat["delivered"]}}</span>
+                        </div>
+                </div>
+         </div>        
+        @endif
+
+                        <div class="mb-2 p-3 bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                                <a href="/mapping" class="bg-pink-800 p-2 text-white rounded-md">Mapping</a>
+                                <a href="/changepwd" class="bg-pink-800 p-2  text-white rounded-md">Change Password</a>
+                        </div>
+                </div>
